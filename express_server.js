1dc2635b6250ae1f85cbcb959321fb5905931968
Kian <kian.akhavan@gmail.com>
2017-06-09 22:17:58 +0000

    tested all user registration functionality ready to merge to master

--- express_server.js
+++ express_server.js
@@ -135,7 +135,7 @@ app.post("/urls/:key/delete", (req, res) => {
 });
 
 app.post("/logout", (req,res) => {
-  res.clearCookie("user");
+  res.clearCookie("user_id");
   res.redirect("/urls");
 });
 
